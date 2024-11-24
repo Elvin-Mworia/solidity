af2dad0ff5d936088f8778056a8e764503965249
Elvin-Mworia <elvinmworia@gmail.com>
2024-11-24 13:36:15 +0300

    installed foundry to use the console class

--- storageVariables.sol
+++ storageVariables.sol
@@ -1,11 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
+import "forge-std/console.sol";
+//install foundry with npm init -y && npm install @foundry-rs/forge --save-dev
 
- contract Example{
+contract Example{
     uint256 a=100;
     uint256 b=type(uint256).max;
 
     bool c= true;
 
-    
+    constructor(){
+        uint256 x;
+        assembly{
+            x:=sload(0x00)
+        }
+        console.log(x);
+
+
+    }
+
+
  }
